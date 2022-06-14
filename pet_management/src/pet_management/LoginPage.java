8bd5d4d45ca1cac379fc96ee758dc8e5ac3f8023
Pyotato <pyolovely01@gmail.com>
2022-06-14 18:26:02 +0900

    사진 경로 파일 업데이트

--- pet_management/src/pet_management/LoginPage.java
+++ pet_management/src/pet_management/LoginPage.java
@@ -23,8 +23,8 @@ public class LoginPage extends JFrame{
 		BufferedImage myPicture;
 		BufferedImage myPicture2;
 		try {
-			myPicture = ImageIO.read(new File("C:\\STUDY\\pet_management\\id.png"));	
-			myPicture2 = ImageIO.read(new File("C:\\STUDY\\pet_management\\pwd.png"));
+			myPicture = ImageIO.read(new File("C:\\git\\pet_management\\id.png"));	
+			myPicture2 = ImageIO.read(new File("C:\\git\\pet_management\\pwd.png"));
 			JLabel picLabel = new JLabel(new ImageIcon(myPicture));
 			JLabel picLabel2 = new JLabel(new ImageIcon(myPicture2));
 			add(picLabel);
